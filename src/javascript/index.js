07add60dbc702737d6c3678c3b5980f79440985a
Martvie <marviedias@gmail.com>
2022-03-15 16:47:22 -0300

    snake move

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
 
+let direction = 'right'
 
 let snake = []
 snake[0] = {
@@ -16,10 +17,31 @@ function createBg() {
 
 function snakeInit() {
     for(let i = 0; i < snake.length; i++){
-        context.fillStyle = 'red'
+        context.fillStyle = 'orange'
         context.fillRect(snake[i].x, snake[i].y, box, box)
     } 
 }
 
-createBg()
-snakeInit()
+function gameInit(){
+    createBg()
+    snakeInit()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+
+    if(direction == 'right'){snakeX += box}
+    if(direction == 'left'){snakeX -= box}
+    if(direction == 'up'){snakeY -= box}
+    if(direction == 'down'){snakeY += box}
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.pop()
+    snake.unshift(newHead)
+}
+
+let game = setInterval(gameInit, 100)

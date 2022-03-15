de4bd66cd9c984f931cbb0e2923b4fd44a2cb5e9
Martvie <marviedias@gmail.com>
2022-03-15 16:29:35 -0300

    snake creation

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,9 +2,24 @@ let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
 
+
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 function createBg() {
     context.fillStyle = 'lightgreen'
     context.fillRect(0,0, 16 * box, 16 * box)
 }
 
+function snakeInit() {
+    for(let i = 0; i < snake.length; i++){
+        context.fillStyle = 'red'
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    } 
+}
+
 createBg()
+snakeInit()

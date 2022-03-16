af05449aa22e9b7273ae55f46872dffa9e755dfd
Martvie <marviedias@gmail.com>
2022-03-16 13:17:32 -0300

    Game Over!

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -57,6 +57,13 @@ function gameInit() {
     if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
     if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game)
+            alert('Fim de JOGO!😥')
+        }
+    }
+
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 

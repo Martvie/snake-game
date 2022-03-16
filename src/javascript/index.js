d6ef53a11b4a502c1efc70b6ee5f0f227607862a
Martvie <marviedias@gmail.com>
2022-03-16 13:09:28 -0300

    food function

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -66,12 +66,19 @@ function gameInit() {
     if (direction == 'up') { snakeY -= box }
     if (direction == 'down') { snakeY += box }
 
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop()
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1 ) * box
+        food.y = Math.floor(Math.random() * 15 + 1 ) * box
+
+    }
+
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
-    snake.pop()
     snake.unshift(newHead)
 
 }
